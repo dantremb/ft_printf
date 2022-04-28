cbec4ff4325fb5b5e53ee23cea30cff8d73c314b
dantremb <dany.tremblay@gmail.com>
2022-04-28 18:15:43 -0400

    dantremb

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 11:19:53 by dantremb          #+#    #+#             */
-/*   Updated: 2022/04/28 17:14:55 by dantremb         ###   ########.fr       */
+/*   Updated: 2022/04/28 18:14:23 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	ft_len(unsigned long nbr, int base)
 	return (len);
 }
 
-int	ft_put_ptr(unsigned long ptr, int base)
+int	ft_put_ptr(unsigned long ptr)
 {
 	ft_put_str("0x");
 	if (ptr == 0)
